8a38e0f61f5577a01abb83cd2abf1a969cfe8bb3
Markus Poeschl <Poeschl@users.noreply.github.com>
2021-04-10 23:48:37 +0100

    Adjust XY voltage

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2496,7 +2496,7 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       975        // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       990        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16        // 0..256
     #define X_RSENSE          0.11
@@ -2514,7 +2514,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       975
+    #define Y_CURRENT       990
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
